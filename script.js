330f61cd77e3b567b32ab29aa2fb80d4742b4192
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2022-09-26 22:37:49 -0300

    manipulando-DOM

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const frutas = ["laranja", "limão", "uva"];
+
+document.getElementById("fruta-1").innerHTML = frutas[0]
+document.getElementById("fruta-2").innerHTML = frutas[1]
+document.getElementById("fruta-3").innerHTML = frutas[2]
+
+let fruta = document.getElementById("fruta-4")
+
+let input = document.getElementById("input")
+
+function adcionar(){
+    fruta.innerHTML = input.value 
+}
+
